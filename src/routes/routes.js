d6c2a73174d63e44837de14b7dc236b552ed4b4c
Rafael Santos <rafaeldsantos16@outlook.com>
2021-03-22 18:16:12 -0300

    Backend com Update e Delete

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,5 +5,7 @@ const router = express.Router();
 router.post('/cliente', ClienteController.Insert);
 router.get('/cliente', ClienteController.SelectAll);
 router.get('/cliente/:id', ClienteController.SelectDetail);
+router.put('/cliente/:id', ClienteController.Update);
+router.delete('/cliente/:id', ClienteController.Delete);
  
 module.exports = router;
